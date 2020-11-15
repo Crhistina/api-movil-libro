7f6dd3a0f6fea25c35fb841e6a0cb0bebb8e886c
Crhistina <crhistina.javier.15@gmail.com>
2020-11-14 21:46:50 -0500

    Mi primer commit

---
+++ src/main/java/com/proyecto/ExamenPc021Application.java
@@ -0,0 +1,13 @@
+package com.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPc021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenPc021Application.class, args);
+	}
+
+}
